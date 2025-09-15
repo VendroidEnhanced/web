5fbd7128eed081575b377c50ed7742ee9e144114
nin0 <personal@nin0.dev>
2025-09-14 20:42:38 -0400

    make say command use cv2

--- src/bot/commands/say.ts
+++ src/bot/commands/say.ts
@@ -1,3 +1,4 @@
+import { ComponentTypes, MessageFlags } from "oceanic.js";
 import { defineCommand } from "../../types";
 
 export default defineCommand({
@@ -5,7 +6,13 @@ export default defineCommand({
     description: "Say something",
     async exec(msg) {
         await msg.channel?.createMessage({
-            content: msg.content.replace(`${process.env.PREFIX}say `, "")
+            flags: MessageFlags.IS_COMPONENTS_V2,
+            components: [
+                {
+                    type: ComponentTypes.TEXT_DISPLAY,
+                    content: msg.content.replace(`${process.env.PREFIX}say `, "")
+                }
+            ]
         });
         return null;
     }
